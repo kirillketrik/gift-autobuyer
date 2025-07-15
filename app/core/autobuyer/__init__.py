f4e41c492cad2e32eb598a746b4581a51d6a41c8
kirillketrik <kirillketrik34@gmail.com>
2025-07-15 11:30:38 +0300

    Move interfaces to core

---
+++ app/core/autobuyer/__init__.py
@@ -0,0 +1,13 @@
+from .autobuyer import Autobuyer
+from .balance_provider import BalanceProvider
+from .gift_allocator import GiftAllocator
+from .gift_provider import GiftProvider
+from .notifier import Notifier
+
+__all__ = [
+    "Autobuyer",
+    "BalanceProvider",
+    "GiftAllocator",
+    "GiftProvider",
+    "Notifier",
+]
