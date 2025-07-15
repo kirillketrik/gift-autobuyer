2b4fd3c274daa12b7d20fb27497b08e922a0138c
kirillketrik <kirillketrik34@gmail.com>
2025-07-15 12:06:37 +0300

    Update imports

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,3 +1,11 @@
-from .allocation import GiftAllocation
 from .gift import Gift
+from .gift_allocation import GiftAllocation
 from .gift_filter import GiftFilter
+from .receiver import Receiver
+
+__all__ = [
+    "GiftAllocation",
+    "Gift",
+    "GiftFilter",
+    "Receiver",
+]
